12340bf30327960c71eda2eef37e515f92495875
marolive <xxmarcio07xx@gmail.com>
2023-09-02 19:47:16 -0300

    inicio

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marolive <marolive@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/02 17:59:26 by marolive          #+#    #+#             */
+/*   Updated: 2023/09/02 19:02:41 by marolive         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie()
+{
+    return;
+}
+
+Zombie::~Zombie()
+{
+    return;
+}
+
+std::string Zombie::getZombieName()
+{
+    return this->_name;
+}
+
+void Zombie::setZombieName(std::string name)
+{
+    this->_name = name;
+}
+
+void Zombie::announce(void)
+{
+    std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ ex00/Zombie.hpp
@@ -0,0 +1,19 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+    public:
+        Zombie();
+        ~Zombie();
+        std::string getZombieName();
+        void setZombieName(std::string name);
+        void announce(void);
+    private:
+        std::string _name;
+ 
+};
+
+#endif
